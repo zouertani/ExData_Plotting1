a033acd5bce34485abf51dcf33c7f88865ce3e3b
zouertani <zouertani@gmail.com>
2016-10-07 15:02:27 +0400

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,17 @@
+## package sqldf is required to use read.csv.sql
+library(sqldf)
+
+## Reading and loading data only from the dates 2007-02-01 and 2007-02-02
+## Read File Filtered by SQL
+EPC <- read.csv.sql("./ExData_Plotting1/household_power_consumption.txt", sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007' ", sep=';', nrows=2075259, stringsAsFactors=F, eol = "\n" ); closeAllConnections()
+
+## Converting the Date and Time variables to Date/Time classes 
+EPC$Date <- as.Date(EPC$Date, format="%d/%m/%Y")
+EPC$DateTime <- as.POSIXct(paste(EPC$Date,EPC$Time))
+
+## plotting
+EPC$Global_active_power <- as.numeric(EPC$Global_active_power)
+plot(EPC$Global_active_power~EPC$DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+## Saving to png file
+dev.copy(png, file="./ExData_Plotting1/plot2.png", height=480, width=480)
+dev.off()

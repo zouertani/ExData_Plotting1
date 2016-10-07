3330c8ca39b09a08aee67d141e64214c0d25f767
zouertani <zouertani@gmail.com>
2016-10-07 07:24:21 +0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ library(sqldf)
 
 ## Reading and loading data only from the dates 2007-02-01 and 2007-02-02
 ## Read File Filtered by SQL
-EPC <- read.csv.sql("./data/household_power_consumption.txt", sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007' ", sep=';', nrows=2075259, stringsAsFactors=F, eol = "\n" ); closeAllConnections()
+EPC <- read.csv.sql("./ExData_Plotting1/household_power_consumption.txt", sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007' ", sep=';', nrows=2075259, stringsAsFactors=F, eol = "\n" ); closeAllConnections()
 
 ## Converting the Date and Time variables to Date/Time classes 
 EPC$Date <- as.Date(EPC$Date, format="%d/%m/%Y")
@@ -12,9 +12,10 @@ EPC$DateTime <- as.POSIXct(paste(EPC$Date,EPC$Time))
 ## plotting
 
 ## Plot 1
-hist(EPC$Global_active_power, main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+EPC$Global_active_power <- as.numeric(EPC$Global_active_power)
+hist(EPC$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 ## Saving to png file
-dev.copy(png, file="./date/plot1.png", height=480, width=480)
+dev.copy(png, file="./ExData_Plotting1/plot1.png", height=480, width=480)
 dev.off()
+

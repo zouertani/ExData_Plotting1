c68dee42709403366b60953564b8939edcd750b5
zouertani <zouertani@gmail.com>
2016-10-07 15:01:19 +0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -18,4 +18,3 @@ hist(EPC$Global_active_power, main="Global Active Power", xlab="Global Active Po
 ## Saving to png file
 dev.copy(png, file="./ExData_Plotting1/plot1.png", height=480, width=480)
 dev.off()
-
